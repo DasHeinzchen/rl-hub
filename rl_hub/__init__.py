d84ffcc96d1f3a5157be672aea4eb950a45093d6
DasHeinzchen <marlon.heinz@outlook.de>
2023-02-05 11:14:56 +0100

    add Config file

--- rl_hub/__init__.py
+++ rl_hub/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from rl_hub.config import Config
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+    app.config.from_object(Config)
 
     from rl_hub.main.routes import main
     app.register_blueprint(main)
